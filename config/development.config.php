522280c0e54ac4c230264e87c06a2599312e89a8
Nicolas Hefti <nicolas@hefti.fr>
2018-06-12 14:48:26 +0200

    Add dev config

---
+++ config/development.config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    // Development time modules
+    'modules' => [
+        'ZendDeveloperTools',
+        'SanSessionToolbar'
+    ],
+
+    'module_listener_options' => [
+        // Turn off caching
+        'config_cache_enabled'     => false,
+        'module_map_cache_enabled' => false,
+    ],
+];
